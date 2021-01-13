37bfcf7a1950eacea3be86cd0f73b6f2d509550d
Jourdan Gundersen <jourdangundersen@Jourdans-MBP.attlocal.net>
2021-01-13 11:59:09 -0600

    stored textarea in local storage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var currentday = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentday").text(currentday);
 console.log("javascript file", currentday);
 $(".btn-primary").on("click", function (){
-    var timeblock = $(this).attr("id");
-    console.log(timeblock);
+    var timeblock = $(this).attr("id").split("-")[0];
+    var userdata = $(`#${timeblock}-text`).val();
+    console.log(timeblock, userdata);
+    localStorage.setItem(timeblock, userdata);
 })
